fcd4c4d70ab058138b455bbf7222c46e2064aeaf
kaidenvlr <kaidenvlr@gmail.com>
2023-11-16 13:13:55 +0600

    login, register, customer-list APIViews

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import parsers
+from rest_framework import permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-# Create your views here.
+from user_app.models import Customer
+from user_app.serializers import RegisterSerializer, CustomerSerializer, LoginSerializer
+
+
+class RegisterAPIView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class LoginAPIView(generics.CreateAPIView):
+    permission_classes = ()
+    throttle_classes = ()
+    parser_classes = (parsers.JSONParser,)
+    serializer_class = LoginSerializer
+
+    def get_serializer_context(self):
+        return {
+            'request': self.request,
+            'format': self.format_kwarg,
+            'view': self
+        }
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['context'] = self.get_serializer_context()
+        return self.serializer_class(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+class CustomerAPIView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(is_verified=True)
+        serializer = self.get_serializer(queryset, many=True)
+        return serializer.data
